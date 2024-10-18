dcb0c17d0219c0b59886f7094592cd05afee92bb
Victoria <victoria_rocha_26@hotmail.com>
2024-10-18 14:18:25 -0300

    definindo atributos das entidades

--- app.py
+++ app.py
@@ -3,14 +3,34 @@ import pymysql # type: ignore
 pymysql.install_as_MySQLdb()
 
 class Usuario():
+    # PK INT - id_usuario
+    # int - CPF 
+    # varchar - nome
+    # varchar - email
+    # varchar - endereço
+    # varchar - Cidade
+    # int - telefone
     pass
 
 class Animais():
+    # PK int - id_animal
+    # varchar - nome
+    # varchar - especie
+    # varchar - região
+    # date - data de entrada na ong
+    # varchar - status apadrinhamento
     pass
 
 class Volutarios():
+    # PK INT -id_voluntário
+    # varchar - interesses
+    # FK INT - id usuario
     pass
 
 class Padrinhos():
+    # PK int - id_apadrinhamento
+    # float - mesada 
+    # FK - id animal
+    # Fk - id_usuario 
     pass
 
