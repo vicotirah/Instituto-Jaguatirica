e025f21fa0d5c3e86fcb77be1c6a27c99cc0bb9f
Victoria <victoria_rocha_26@hotmail.com>
2024-10-18 13:03:14 -0300

    Criando as entidades

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import pymysql # type: ignore
+
+pymysql.install_as_MySQLdb()
+
+class Usuario():
+    pass
+
+class Animais():
+    pass
+
+class Volutarios():
+    pass
+
+class Padrinhos():
+    pass
 
