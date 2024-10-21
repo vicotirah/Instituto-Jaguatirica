a613873b50b5f49bba98a4774046f03f65f41d45
Victória Rocha <victoria_rocha_26@hotmail.com>
2024-10-21 15:39:27 -0300

    Update institutojaguatirica.sql

--- institutojaguatirica.sql
+++ institutojaguatirica.sql
@@ -4,7 +4,9 @@ use ong;
 create table if not exists animal (
     idAnimal INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL ,
-    nome_popular VARCHAR(45) NOT NULL,
+    nome_popular VARCHAR(45) NOT NULL,	
+    sexo VARCHAR(45) NOT NULL,	
+    genero VARCHAR  NOT NULL,	
     especie VARCHAR(45) NOT NULL,
     regiao VARCHAR(45) NOT NULL,
     data_entrada DATE NOT NULL,
@@ -40,17 +42,17 @@ create table if not exists  apadrinhamento (
 	FOREIGN KEY (animal_idAnimal) REFERENCES animal(idAnimal)
 );
 
-INSERT INTO animal (nome, nome_popular, especie, regiao, data_entrada, status_preservacao) VALUES
-('Jacy', 'Onça-Pintada', 'Panthera onca', 'Mata Atlântica', '2023-01-01', 'Ameaçado'),
-('Jade', 'Ararinha-azul', 'Cyanopsitta spixii', 'Cerrado', '2023-01-02', 'Extinto na Natureza'),
-('Tati', 'Mico-leão-dourado', 'Leontopithecus rosalia', 'Mata Atlântica', '2023-01-03', 'Ameaçado'),
-('Tande', 'Tamanduá-bandeira', 'Myrmecophaga tridactyla', 'Cerrado', '2023-01-04', 'Vulnerável'),
-('Iporã', 'Cágado-da-amazônia', 'Podocnemis expansa', 'Amazônia', '2023-01-05', 'Ameaçado'),
-('Guarati', 'Lobo-guará', 'Chrysocyon brachyurus', 'Cerrado', '2023-01-06', 'Vulnerável'),
-('Fuleco', 'Tatu-canastra', 'Priodontes maximus', 'Amazônia', '2023-01-07', 'Ameaçado'),
-('Sombra', 'Gato-do-mato', 'Leopardus wiedii', 'Mata Atlântica', '2023-01-08', 'Vulnerável'),
-('Yara', 'Peixe-boi', 'Trichechus inunguis', 'Amazônia', '2023-01-09', 'Ameaçado'),
-('Pitanga', 'Jaguatirica', 'Leopardus pardalis', 'Mata Atlântica', '2023-01-10', 'Ameaçado');
+INSERT INTO animal (nome, nome_popular, sexo, genero, especie, regiao, data_entrada, status_preservacao) VALUES
+('Jacy', 'Onça-Pintada', 'Fêmea', 'Mamífero', 'Panthera onca', 'Mata Atlântica', '2023-01-01', 'Ameaçado'),
+('Jade', 'Ararinha-azul', 'Fêmea', 'Ave', 'Cyanopsitta spixii', 'Cerrado', '2023-01-02', 'Extinto na Natureza'),
+('Taki', 'Mico-leão-dourado', 'Macho', 'Mamífero', 'Leontopithecus rosalia', 'Mata Atlântica', '2023-01-03', 'Ameaçado'),
+('Tande', 'Tamanduá-bandeira', 'Macho', 'Mamífero', 'Myrmecophaga tridactyla', 'Cerrado', '2023-01-04', 'Vulnerável'),
+('Iporã', 'Cágado-da-amazônia', 'Macho', 'Réptil','Podocnemis expansa', 'Amazônia', '2023-01-05', 'Ameaçado'),
+('Guarati', 'Lobo-guará','Macho', 'Mamífero', 'Chrysocyon brachyurus', 'Cerrado', '2023-01-06', 'Vulnerável'),
+('Fuleco', 'Tatu-canastra', 'Macho', 'Réptil','Priodontes maximus', 'Amazônia', '2023-01-07', 'Ameaçado'),
+('Sombra', 'Gato-do-mato','Fêmea', 'Mamífero', 'Leopardus wiedii', 'Mata Atlântica', '2023-01-08', 'Vulnerável'),
+('Yara', 'Peixe-boi', 'Fêmea', 'Mamífero','Trichechus inunguis', 'Amazônia', '2023-01-09', 'Ameaçado'),
+('Pitanga', 'Jaguatirica', 'Fêmea', 'Mamífero','Leopardus pardalis', 'Mata Atlântica', '2023-01-10', 'Ameaçado');
 
 INSERT INTO usuario (CPF, nome, email, endereco, cidade, estado, tel) VALUES
 ('12345678900', 'João Silva', 'joao.silva@email.com', 'Rua A, 123', 'São Paulo', 'SP', '11912345678'),
