769ac736511bd44432fa411f670b02ccb8b03ec9
Victoria <victoria_rocha_26@hotmail.com>
2024-11-12 12:31:09 -0300

    Arrumando menu, pagina regiões

--- Front/vite-project/src/menu.js
+++ Front/vite-project/src/menu.js
@@ -3,10 +3,13 @@ const btnMenu = document.getElementById("hamburguer-button")
 const menu =document.getElementById("menu")
 
 btnMenu.classList.add("hamburguer-button-js-enabled")
+menu.classList.add("menu-closed")
 btnMenu.setAttribute("aria-expanded", "false")
 menu.setAttribute("aria-hidden", "true")
 
+
 btnMenu.addEventListener("click", function(){
+
     let expanded = this.getAttribute("aria-expanded") === "true" ? true : false
 
     if (expanded){
