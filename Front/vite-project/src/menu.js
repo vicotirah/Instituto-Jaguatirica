7f3ed477f5728b1c948f47767d92813406201fa5
Victoria <victoria_rocha_26@hotmail.com>
2024-11-27 16:18:37 -0300

    Menu Dropdown mobile e desktop, ajuste das rotas

--- Front/vite-project/src/menu.js
+++ Front/vite-project/src/menu.js
@@ -1,4 +1,5 @@
 
+
 const btnMenu = document.getElementById("hamburguer-button")
 const menu =document.getElementById("menu")
 
@@ -6,7 +7,6 @@ btnMenu.classList.add("hamburguer-button-js-enabled")
 menu.classList.add("menu-closed")
 btnMenu.setAttribute("aria-expanded", "false")
 menu.setAttribute("aria-hidden", "true")
-
 
 btnMenu.addEventListener("click", function(){
 
@@ -21,3 +21,20 @@ btnMenu.addEventListener("click", function(){
     this.setAttribute("aria-expanded", !expanded)
     menu.setAttribute("aria-hidden", expanded)
 })
+
+const dropdown = document.getElementById('dropdownAnimais');
+dropdown.addEventListener('click', function() {
+  const subMenu = this.querySelector('.dropdown-menu');
+  subMenu.classList.toggle('show');
+});
+
+const dropdownItem = document.getElementById('dropdownAnimaisDkt');
+const dropdownMenu = dropdownItem.querySelector('.dropdown-menu-dkt');
+dropdownItem.addEventListener('click', function(event) {
+    const isVisible = dropdownMenu.classList.contains('visible'); // Verifica se o menu está visível
+    if (isVisible) {
+        dropdownMenu.classList.remove('visible'); // Esconde o menu
+    } else {
+        dropdownMenu.classList.add('visible'); // Exibe o menu
+    }
+}); 
