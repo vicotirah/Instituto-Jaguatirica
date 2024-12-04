156a736f3c154682f4460d3243be0fa6d3fddccd
Victoria <seuemail@example.com>
2024-12-03 22:35:08 -0300

    FOI

--- Front/vite-project/index.js
+++ Front/vite-project/index.js
@@ -1,14 +1,6 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-function envioVoluntario(){
+function envioVoluntario() {
   alert("Dados enviados!")
 }
-function receberPix(){
+function receberPix() {
   alert("Apadrinhamento selecionado! Mande o comprovante pelo nosso email!")
 }
