b27106a6c8169ad4dcff3d57a042eecbee9ff5a4
guedespedro <pedroguazevedo123@gmail.com>
2024-12-03 21:39:51 -0300

    adição da página de apadrinhamento, resolução do font-size do menu

--- Front/vite-project/index.js
+++ Front/vite-project/index.js
@@ -9,3 +9,6 @@ function myFunction() {
 function envioVoluntario(){
   alert("Dados enviados!")
 }
+function receberPix(){
+  alert("Apadrinhamento selecionado! Mande o comprovante pelo nosso email!")
+}
